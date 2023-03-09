71d26eae57764ed396ebe68a89d2ec9e92d18567
TranThanhHongQuan <126212879+TranThanhHongQuan@users.noreply.github.com>
2023-03-09 12:15:35 +0700

    l

--- chuong3/BTCN/31_BTCNC3_Bai3_8.py
+++ chuong3/BTCN/31_BTCNC3_Bai3_8.py
@@ -1,5 +1,11 @@
 a=input()
 n=int(input())
-for i in range(1,n+1):
-    print(a * i,end=" ")
+i=1
+while i<=n:
+    j=1
+    while j<=i:
+        print(a,end=" ")
+        j+=1
     print()
+    i+=1
+    
